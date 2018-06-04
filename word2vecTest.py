aae236cf1019d37ab5af6d3cc0fa8fa811b934fc
John DeJesus <j.dejesus22@gmail.com>
2018-06-03 21:24:48 -0400

    Kmeans with Matplotlib

--- word2vecTest.py
+++ word2vecTest.py
@@ -8,6 +8,9 @@ import plotly.offline as pyo
 import plotly.plotly as py
 import plotly.graph_objs as go
 
+import numpy as np
+from sklearn.cluster import KMeans
+
 #create some sample sentences
 sentences=[['i', 'am', 'experimenting', 'with', 'words', 'to' , 'vectors'],
            ['i','hope','this','will','yield','the','results','that','I','want'],
@@ -39,6 +42,7 @@ words=[word for word in vocab.keys()]
 x=[word_vectors[word][0] for word in words]
 y=[word_vectors[word][1] for word in words]
 
+'''
 #plot scatter plot with matplotlib
 plt.scatter(x,y)
 plt.title('Word2Vec Text Visual Representation')
@@ -64,6 +68,43 @@ data=[trace]
 
 fig=go.Figure(data=data,layout=layout)
 pyo.iplot(fig)
+'''
+
+#kmeans clustering
+X=np.array([x,y])
+X=X.transpose()
+
+WCSS=[]
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i, init='k-means++',max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    WCSS.append(kmeans.inertia_)
+plt.plot(range(1,11),WCSS)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+#Applying k-means to data
+Num_Clusters=2
+kmeans=KMeans(n_clusters=Num_Clusters, init='k-means++',max_iter=300, n_init=10, random_state=0)
+y_kmeans=kmeans.fit_predict(X)
+
+#Visualizing the Clusters
+plt.scatter(X[y_kmeans==0,0],X[y_kmeans==0,1],s=100,c='red')
+plt.scatter(X[y_kmeans==1,0],X[y_kmeans==1,1],s=100,c='blue')
+'''
+plt.scatter(X[y_kmeans==2,0],X[y_kmeans==2,1],s=100,c='green')
+plt.scatter(X[y_kmeans==3,0],X[y_kmeans==3,1],s=100,c='cyan')
+plt.scatter(X[y_kmeans==4,0],X[y_kmeans==4,1],s=100,c='magenta')
+'''
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,c='yellow',label='Centroids')
+plt.title('Word2vec Kmeans Clustering')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.legend()
+plt.show()
 
 #get similarity score -0.85867835313070628
 model.wv.similarity('hope','results')
