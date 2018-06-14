400c7f5a7435eabce3ce8289119357eeaf73b065
John DeJesus <j.dejesus22@gmail.com>
2018-06-13 22:33:18 -0400

    more recursion practice

--- InterviewQuestionSolutions.py
+++ InterviewQuestionSolutions.py
@@ -204,13 +204,63 @@ test.test_rev(reverse)
 
 ##############################################################################
 
-#given a string, output a list of all permutations
-
-def permutations(s):
-    
-    if len(s)==1:
-        return [s]
+#given a string, output a list of all permutations (needed some help)
+#exceeds limit
+def permute(s):
     
     permutations = []
     
-    return 
+    if len(s)==1:
+        permutations=[s]
+    
+    else:
+        for i, letter in enumerate(s):
+                
+            for perm in permute(s[:1]+s[i+1:]):
+                    
+                permutations+=[letter+perm]
+    
+    
+    return permutations
+
+##############################################################################
+    
+#fibonaci sequence with recursion (own solution)
+
+def fib_rec(n):
+    
+    if n == 0 or n==1:
+        return 1
+    else:
+        return fib_rec(n-1)+fib_rec(n-2)
+    
+#fibonaci sequence with iter(from course)
+        
+def fib_iter(n):
+    
+    a,b=0,1
+    
+    for i in range(n):
+        a,b=b,a+b
+
+    return a
+
+#fibonaci sequence with memiozation (from course)
+n=10
+cache=[None]*(n+1)
+def fib_dyn(n):
+    
+    #base case
+    if n == 0 or n==1:
+        return 1
+    
+    #check cache
+    if cache[n]!= None:
+        return cache[n]
+    
+    #keep cache
+    cache[n]=fib_dyn(n-1)+fib_dyn(n-2)
+    
+    return cache[n]
+    
+    
